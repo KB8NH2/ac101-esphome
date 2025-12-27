11c10f2918845977da11d3a007cc232d98bdf5b9
KB8NH2 <141185054+KB8NH2@users.noreply.github.com>
2025-12-27 12:59:27 -0500

    Implement AC101 component with schema and code
    Added AC101 component with configuration schema and code generation.

--- components/ac101/__init__.py
+++ components/ac101/__init__.py
@@ -1,1 +1,14 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import core
 
+ac101_ns = cg.esphome_ns.namespace("ac101")
+AC101 = ac101_ns.class_("AC101", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(AC101),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[core.CONF_ID])
+    await cg.register_component(var, config)
